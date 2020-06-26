fb2f5abdd68fcf6aae7332563fc29b59b83265bc
krsign <nowimkr@gmail.com>
2020-06-26 15:09:13 +0530

    initial commit

--- account/admin.py
+++ account/admin.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 
 # from account.models import User
+
 from django.contrib.auth.admin import UserAdmin
 
 admin.site.register(get_user_model(), UserAdmin)
