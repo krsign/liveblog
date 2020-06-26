a14551de615fdc73280f989106061a20b166aa51
krsign <nowimkr@gmail.com>
2020-06-26 23:55:44 +0530

    initial commit

--- account/views.py
+++ account/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
-from account.forms import SignUpForm
+from django.views.generic import CreateView, UpdateView
+from account.forms import SignUpForm, UpdateForm
 from django.urls import reverse_lazy
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate, login
 class SignUpView(CreateView):
     model = settings.AUTH_USER_MODEL
@@ -18,3 +19,16 @@ class SignUpView(CreateView):
         user = authenticate(username=cd['username'], password=cd['password1'])
         login(self.request, user)
         return result
+
+def accountView(request):
+    user = request.user
+
+    context = {'user': user}
+    return render(request, 'registration/account.html', context)
+
+class UserUpdate(UpdateView):
+    model = get_user_model()
+    form_class = UpdateForm
+    pk_url_kwarg = 'id'
+    template_name = 'registration/update.html'
+    success_url = reverse_lazy('profile')
